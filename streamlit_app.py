ce5ab86c60376d4fd69811f7a5dac0040b519bf3
jusungLee12 <158030484+jusungLee12@users.noreply.github.com>
2026-05-21 00:23:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def plot_advanced_sunspot_visualizations(df, sunactivity_col='SUNACTIVITY'):
     return fig
 
 # 메인 앱
-st.title('🌞 태양흑점 데이터 분석 대시보드🌞')
+st.title('🌞 태양흑점 데이터 분석 대시보드 🌞')
 st.markdown("""
     이 대시보드는 태양흑점 데이터를 다양한 시각화 방법으로 보여줍니다.
     """)
